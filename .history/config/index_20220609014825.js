c67e6433ecf2714210e8da5c97398f8f5aef8a5d
Busayo Samuel <busayosamuel2016@gmail.com>
2022-09-17 11:40:00 +0100

    updated dev env

--- .history/config/index_20220609014825.js
+++ .history/config/index_20220609014825.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev 
     ? "http://localhost:3000" 
-    : "https://food-commerce-eosin.vercel.app/";
+    : "https://nest-nextjs.vercel.app";
